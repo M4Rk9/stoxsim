fb4294bacfbbf79df47c8d58658bd3bc96516da5
Marky <85974485+M4Rk9@users.noreply.github.com>
2026-07-22 19:03:33 +0530

    Add backend/src/main/java/com/stoxsim/StoxSimApplication.java

---
+++ backend/src/main/java/com/stoxsim/StoxSimApplication.java
@@ -0,0 +1,12 @@
+package com.stoxsim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoxSimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoxSimApplication.class, args);
+    }
+}
